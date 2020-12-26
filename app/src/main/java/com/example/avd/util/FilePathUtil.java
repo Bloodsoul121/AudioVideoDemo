a80959d4d7aea1dcec2f11338fbe9a4bde5cfe41
bloodsoul <2312178690@qq.com>
2020-12-26 15:06:53 +0800

    commit 输出h264字节文件

---
+++ app/src/main/java/com/example/avd/util/FilePathUtil.java
@@ -0,0 +1,116 @@
+package com.example.avd.util;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+
+import com.example.avd.BuildConfig;
+
+import java.io.File;
+
+import androidx.core.content.FileProvider;
+
+public class FilePathUtil {
+
+    public static String parseUri(final Context context, final Uri uri) {
+        String photoPath = "";
+        if (context == null || uri == null) {
+            return photoPath;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
+            String docId = DocumentsContract.getDocumentId(uri);
+            if (isExternalStorageDocument(uri)) {
+                String[] split = docId.split(":");
+                if (split.length >= 2) {
+                    String type = split[0];
+                    if ("primary".equalsIgnoreCase(type)) {
+                        photoPath = Environment.getExternalStorageDirectory() + "/" + split[1];
+                    }
+                }
+            } else if (isDownloadsDocument(uri)) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                photoPath = getDataColumn(context, contentUri, null, null);
+            } else if (isMediaDocument(uri)) {
+                String[] split = docId.split(":");
+                if (split.length >= 2) {
+                    String type = split[0];
+                    Uri contentUris = null;
+                    if ("image".equals(type)) {
+                        contentUris = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("video".equals(type)) {
+                        contentUris = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("audio".equals(type)) {
+                        contentUris = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    }
+                    String selection = MediaStore.Images.Media._ID + "=?";
+                    String[] selectionArgs = new String[]{split[1]};
+                    photoPath = getDataColumn(context, contentUris, selection, selectionArgs);
+                }
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            photoPath = uri.getPath();
+        } else {
+            photoPath = getDataColumn(context, uri, null, null);
+        }
+
+        return photoPath;
+    }
+
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        if (null == uri) return null;
+
+        final String scheme = uri.getScheme();
+        String data = null;
+
+        if (scheme == null)
+            data = uri.getPath();
+        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
+    }
+
+    private Uri getUriFromFile(Context context, File file) {
+        if (context == null || file == null) {
+            throw new NullPointerException();
+        }
+        Uri uri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            uri = FileProvider.getUriForFile(context.getApplicationContext(), BuildConfig.APPLICATION_ID + ".fileprovider", file);
+        } else {
+            uri = Uri.fromFile(file);
+        }
+        return uri;
+    }
+
+}
--- app/src/main/java/com/example/avd/util/FileUtil.java
+++ app/src/main/java/com/example/avd/util/FileUtil.java
@@ -1,115 +1,72 @@
 package com.example.avd.util;
 
-import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
-import androidx.core.content.FileProvider;
+/*
+ *  @项目名：  AudioVideoDemo
+ *  @包名：    com.example.avd.util
+ *  @文件名:   FileUtil
+ *  @创建者:   bloodsoul
+ *  @创建时间:  2020/12/26 14:51
+ *  @描述：    TODO
+ */
 
-import com.example.avd.BuildConfig;
+import android.util.Log;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileUtil {
 
-    public static String parseUri(final Context context, final Uri uri) {
-        String photoPath = "";
-        if (context == null || uri == null) {
-            return photoPath;
+    private static final String TAG = "FileUtil";
+
+    public static void writeContent(byte[] array, File saveFile) {
+        char[] HEX_CHAR_TABLE = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        StringBuilder sb = new StringBuilder();
+        for (byte b : array) {
+            // 高位
+            sb.append(HEX_CHAR_TABLE[(b & 0xf0) >> 4]);
+            // 低位
+            sb.append(HEX_CHAR_TABLE[b & 0x0f]);
         }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
-            String docId = DocumentsContract.getDocumentId(uri);
-            if (isExternalStorageDocument(uri)) {
-                String[] split = docId.split(":");
-                if (split.length >= 2) {
-                    String type = split[0];
-                    if ("primary".equalsIgnoreCase(type)) {
-                        photoPath = Environment.getExternalStorageDirectory() + "/" + split[1];
-                    }
+        Log.i(TAG, "writeContent: " + sb.toString());
+        FileWriter writer = null;
+        try {
+            // 打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
+            writer = new FileWriter(saveFile, true);
+            writer.write(sb.toString());
+            writer.write("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
                 }
-            } else if (isDownloadsDocument(uri)) {
-                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
-                photoPath = getDataColumn(context, contentUri, null, null);
-            } else if (isMediaDocument(uri)) {
-                String[] split = docId.split(":");
-                if (split.length >= 2) {
-                    String type = split[0];
-                    Uri contentUris = null;
-                    if ("image".equals(type)) {
-                        contentUris = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                    } else if ("video".equals(type)) {
-                        contentUris = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                    } else if ("audio".equals(type)) {
-                        contentUris = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                    }
-                    String selection = MediaStore.Images.Media._ID + "=?";
-                    String[] selectionArgs = new String[]{split[1]};
-                    photoPath = getDataColumn(context, contentUris, selection, selectionArgs);
-                }
-            }
-        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            photoPath = uri.getPath();
-        } else {
-            photoPath = getDataColumn(context, uri, null, null);
-        }
-
-        return photoPath;
-    }
-
-    private static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    private static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    private static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
-        if (null == uri) return null;
-
-        final String scheme = uri.getScheme();
-        String data = null;
-
-        if (scheme == null)
-            data = uri.getPath();
-        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
-            data = uri.getPath();
-        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
-            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
-            if (null != cursor) {
-                if (cursor.moveToFirst()) {
-                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-                    if (index > -1) {
-                        data = cursor.getString(index);
-                    }
-                }
-                cursor.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        return data;
     }
 
-    private Uri getUriFromFile(Context context, File file) {
-        if (context == null || file == null) {
-            throw new NullPointerException();
+    public static void writeBytes(byte[] array, File saveFile) {
+        FileOutputStream writer = null;
+        try {
+            // 打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
+            writer = new FileOutputStream(saveFile, true);
+            writer.write(array);
+            writer.write('\n');
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        Uri uri;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            uri = FileProvider.getUriForFile(context.getApplicationContext(), BuildConfig.APPLICATION_ID + ".fileprovider", file);
-        } else {
-            uri = Uri.fromFile(file);
-        }
-        return uri;
     }
 
 }
