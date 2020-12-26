2d3a15fc124dfc9246eab0feefd0263e22e7d74d
bloodsoul <2312178690@qq.com>
2020-12-26 15:53:53 +0800

    commit h264 sps

---
+++ app/src/main/java/com/example/test/Ue.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+/*
+ *  @项目名：  AudioVideoDemo
+ *  @包名：    com.example.test
+ *  @文件名:   Ue
+ *  @创建者:   bloodsoul
+ *  @创建时间:  2020/12/26 15:41
+ *  @描述：    TODO
+ */
+
+class Ue {
+
+    public static void main(String[] args) {
+        System.out.println("height:  ");
+    }
+
+}
