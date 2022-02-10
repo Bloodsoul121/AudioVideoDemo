7654d754bb510e4eaef771b4a286da291b113cdf
caiguangzu.cgz <caiguangzu.cgz@alibaba-inc.com>
2022-02-10 15:24:26 +0800

    接入ffmpeg，并能成功调用api

--- ffmpeg/src/main/cpp/ffmpeg-lib.cpp
+++ ffmpeg/src/main/cpp/ffmpeg-lib.cpp
@@ -9,6 +9,12 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG,__VA_ARGS__)
 
 extern "C" {
-
+#include <libavcodec/avcodec.h>
 }
 
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_cgz_ffmpeg_FfmpegActivity_testFfmpeg(JNIEnv *env, jobject thiz) {
+    std::string config = avcodec_configuration();
+    return env->NewStringUTF(config.c_str());
+}
--- ffmpeg/src/main/java/com/cgz/ffmpeg/FfmpegActivity.kt
+++ ffmpeg/src/main/java/com/cgz/ffmpeg/FfmpegActivity.kt
@@ -1,11 +1,23 @@
 package com.cgz.ffmpeg
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class FfmpegActivity : AppCompatActivity() {
+
+    companion object {
+        init {
+            System.loadLibrary("ffmpeg-lib")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ffmpeg)
+        findViewById<TextView>(R.id.tv).text = testFfmpeg()
     }
+
+    private external fun testFfmpeg(): String
+
 }
